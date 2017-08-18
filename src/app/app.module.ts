ba5124287351d21e0013cf287e8320de8c3b421d
Siddharth Mishra <siddharthm@chetu.com>
2017-08-18 22:40:37 +0530

    Added login register component and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {path:'', redirectTo:'login', pathMatch:"full"},
+  {path:'login', component:LoginComponent},
+  {path:'register', component:RegisterComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
